dfe327ee0bd5c1c93827fca6c089d3e3d4596db8
DaikiShiroma <daiki20000417@gmail.com>
2023-04-19 11:39:53 +0900

    ログイン状態を保持する機能を追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -28,5 +28,6 @@ class RegistForm(forms.ModelForm):
 class UserLoginForm(AuthenticationForm):
     username = forms.EmailField(label='メールアドレス')
     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+    remember = forms.BooleanField(label='ログイン状態を保持する', required=False)
 
 
--- login_view/settings.py
+++ login_view/settings.py
@@ -125,6 +125,8 @@ LOGIN_URL = '/accounts/user_login'
 LOGIN_REDIRECT_URL = "/accounts/home"
 LOGOUT_REDIRECT_URL = "/accounts/user_login"
 
+SESSION_COOKIE_AGE = 5
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
